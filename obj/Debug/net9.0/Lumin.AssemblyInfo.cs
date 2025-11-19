fc7a082e2aa7f03583a3de94017c74e1677cb8bf
nycolassena01 <nycolassena01@gmail.com>
2025-11-19 20:19:03 -0300

    Sua mensagem de commit

--- obj/Debug/net9.0/Lumin.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lumin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lumin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc216cc4a12fac76e09c156040712354fafc844e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c7162aaee1e1cf5b821930e0e0e0a452e5a388")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lumin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lumin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
