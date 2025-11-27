571c18e6e9ba873ba7ebe8bee0e96788c9155cb7
nycolassena01 <nycolassena01@gmail.com>
2025-11-27 20:26:09 -0300

    SUBMIT

--- obj/Debug/net9.0/Lumin.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lumin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lumin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553459b5b7b1bf2095ec1391f566e02a94b8fd0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01c63172cd3eeea72fb79e965403d02d7d19689")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lumin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lumin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
