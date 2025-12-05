2089c215c3617b1eaf87821ae16e382521c3ac22
MrMaicke <marcelosantiago944@gmail.com>
2025-12-04 21:58:17 -0300

    correção da confirmação da senha

--- Models/Perfil.cs
+++ Models/Perfil.cs
@@ -1,11 +1,27 @@
+using System;
+using System.Collections.Generic;
+
 namespace Lumin.Models
 {
     public class Perfil
     {
-        public int Id { get; set; }
-        public int UsuarioId { get; set; } // identifica cada usuário
         public string Nome { get; set; }
-        public string Bio { get; set; }
-        public string Avatar { get; set; } // caminho da imagem
+        public string Profissao { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Empresa { get; set; }
+        public string Avatar { get; set; }
+
+        public List<Post> Posts { get; set; } = new List<Post>();
+        public List<Perfil> Amigos { get; set; } = new List<Perfil>();
+        public List<Perfil> PedidosAmizade { get; set; } = new List<Perfil>();
+    }
+
+    public class Post
+    {
+        public string Conteudo { get; set; }
+        public string Imagem { get; set; }
+        public string Autor { get; set; }
+        public DateTime Data { get; set; }
     }
 }
